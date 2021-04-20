e6d3443cddc06f009fc5791ff159ef7f7ca3854a
unknown <kprathik@gmail.com>
2021-04-20 17:19:13 -0400

    Revert "GBM Attention file"
    This reverts commit 77e66fa7d0cf7f7a63d8e59b5ea322ddf7eb8bc3.
    Removed attention stuff

--- code/classes/cryptocurrency.py
+++ code/classes/cryptocurrency.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 class Crypto:
     def __init__(self,symbol):
-        data_path = "data/"+symbol.upper()+".csv"
+        data_path = "data/"+symbol.upper()+"_USD.csv"
         self.symbol = symbol.upper()
         self.price_df = pd.read_csv(data_path)
         self.n_datapoints = len(self.price_df)
