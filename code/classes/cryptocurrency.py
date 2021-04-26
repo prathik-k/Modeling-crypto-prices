a1e863863a0fcbd8b823d520de04638f741f064a
SaurabhDoodhwala <saurabh.doodhwala@gmail.com>
2021-04-25 22:45:41 -0400

    Recent main

--- code/classes/cryptocurrency.py
+++ code/classes/cryptocurrency.py
@@ -4,19 +4,31 @@ from datetime import timedelta
 class Crypto:
     def __init__(self,symbol):
         data_path = "data/"+symbol.upper()+"_USD.csv"
+        attention_data_path = "data/"+symbol.upper()+".csv"
         self.symbol = symbol.upper()
         self.price_df = pd.read_csv(data_path)
+        self.attention_df = pd.read_csv(attention_data_path)
         self.n_datapoints = len(self.price_df)
 
     @property
     def get_df(self):
         return self.price_df
+
+    @property
+    def get_attention_df(self):
+        return self.attention_df
     
-    
+    '''
+    def return_prices_over_range(self,start_date='2018-05-01',end_date='2019-05-01'):
+        if isinstance(end_date,int):
+            end_date = start_date+timedelta(days=end_date)
+        self.price_df['Date'] = pd.to_datetime(self.price_df['Date'])
+        mask = (self.price_df['Date'] > start_date) & (self.price_df['Date'] <= end_date)
+        return self.price_df.loc[mask]
+    '''
     def return_prices_over_range(self,start_date='2018-05-01',end_date='2019-05-01'):
         if isinstance(end_date,int):
             end_date = start_date+timedelta(days=end_date)
         self.price_df['Date'] = pd.to_datetime(self.price_df['Date'])
         mask = (self.price_df['Date'] > start_date) & (self.price_df['Date'] <= end_date)
         return self.price_df.loc[mask]['Closing Price (USD)']
-    
--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 from classes.cryptocurrency import Crypto
 from classes.GBM_base import GBM_base
+from classes.GBM_attention import GBM_attention
 import argparse
 
 if __name__=='__main__':
@@ -7,14 +8,17 @@ if __name__=='__main__':
      historic range, type of prediction and prediction details.""")
 
     '''
-    Sample command for a single prediction:
-    python main.py --crypto btc --prediction single --n_paths 100 --h_range "2018-06-06,2019-06-06" --n_pred_pts 100 --save_res Y
+    Sample command for a single prediction and base model:
+    python main.py --crypto btc --model base --prediction single --n_paths 100 --h_range "2018-06-06,2019-06-06" --n_pred_pts 100 --save_res Y
+    Sample command for a rolling prediction and base model:
+    python main.py --crypto btc --model base --prediction rolling --n_paths 100 --h_range "2014-06-06,2018-06-06" --n_periods 10 --period 30 --save_res Y
+    Sample command for a single prediction and attention model:
+    python main.py --crypto btc --model attention --prediction rolling --n_paths 100 --h_range "2014-06-06,2018-06-06" --n_periods 10 --period 30 --save_res Y
 
-    Sample command for a rolling prediction:
-    python main.py --crypto btc --prediction rolling --n_paths 100 --h_range "2014-06-06,2018-06-06" --n_periods 10 --period 30 --save_res Y
     '''
     
     parser.add_argument('--crypto',action='store',type=str,required=True,help='Cryptocurrency')
+    parser.add_argument('--model',action='store',type=str,required=True,help='Type of Model (Base/Attention)')
     parser.add_argument('--prediction',action='store',default='single',type=str,required=True,help='Type of prediction (single/rolling)')
     parser.add_argument('--n_paths',action='store',default=100,type=int,required=True,help='Number of random paths generated')
     parser.add_argument('--h_range',action='store',default=["2018-06-06","2019-06-06"],
@@ -30,22 +34,28 @@ if __name__=='__main__':
     help='Save plot of prediction results? (Y/N)')
     args=parser.parse_args()
 
-    crypto,pred_type,n_paths,save_res=args.crypto,args.prediction,args.n_paths,args.save_res
+    crypto,model,pred_type,n_paths,save_res=args.crypto,args.model,args.prediction,args.n_paths,args.save_res
 
     index = Crypto(crypto.upper())
-    if pred_type=='single':
+    if pred_type=='single' and model=="base":
         hist_range,n_pred_pts = args.h_range,args.n_pred_pts        
         gbm = GBM_base(crypto=index,hist_range=hist_range,pred_type=pred_type)
         gbm.make_predictions_base(n_pred_paths=n_paths,n_pred=n_pred_pts)
         gbm.plot_predictions(savefig=True) if save_res.upper()=='Y' else gbm.plot_predictions(savefig=False)
     
-    elif pred_type=='rolling':
+    elif pred_type=='rolling' and model=="base":
         hist_range,period,n_periods = args.h_range,args.period,args.n_periods
         
         gbm = GBM_base(crypto=index,hist_range=hist_range,period=period,n_pred_periods=n_periods,pred_type=pred_type)
         gbm.make_predictions_base(n_pred_paths=n_paths)
         gbm.plot_predictions(savefig=True) if save_res.upper()=='Y' else gbm.plot_predictions(savefig=False)
-    
+
+    elif pred_type=='single' and model=="attention":
+        hist_range,period,n_periods,n_pred_pts = args.h_range,args.period,args.n_periods,args.n_pred_pts
+        gbm = GBM_attention(crypto=index,hist_range=hist_range,period=period,n_pred_periods=n_periods,pred_type=pred_type)
+        gbm.make_predictions_base(n_pred_paths=n_paths,n_pred=n_pred_pts)
+        gbm.plot_predictions(savefig=True) if save_res.upper()=='Y' else gbm.plot_predictions(savefig=False)
+
 '''
     btc = Crypto('ETH')
     gbm = GBM_base(crypto=btc,hist_range=[0,1000],
@@ -53,5 +63,3 @@ if __name__=='__main__':
     gbm.make_predictions_base(n_pred_paths=100,n_pred=300)
     gbm.plot_predictions(savefig=True)
 '''
-
-
